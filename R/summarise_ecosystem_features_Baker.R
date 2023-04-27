2cb8b0efbac06d883ae63b5e7f7e78c5dfb94c39
luzvpascal <luz.pascal96@gmail.com>
2023-04-27 11:15:08 +1000

    temporary fix baker model

--- R/summarise_ecosystem_features_Baker.R
+++ R/summarise_ecosystem_features_Baker.R
@@ -13,10 +13,13 @@ summarise_ecosystem_features_Baker <- function(parameters,sim_args){
   n_species <- sim_args$n_species
 
   #reconstruct interaction matrix and vector of growth rates from array of parameters
-  reconstruct <- EEMtoolbox::reconstruct_matrix_growthrates_Baker(parameters,sim_args)
+  # reconstruct <- EEMtoolbox::reconstruct_matrix_growthrates_Baker(parameters,sim_args)
+  reconstruct <- EEMtoolbox::reconstruct_matrix_growthrates(parameters,sim_args)
   r <- reconstruct$growthrates
-  A <- reconstruct$alphas_matrix
-  B <- reconstruct$interaction_matrix
+  mat <- reconstruct$interaction_matrix
+
+  A <- pmax(0, mat)
+  B <- pmin(0, mat)
 
   # FEASIBILITY CHECK
   #find equilibrium abundances for feasibility
@@ -25,17 +28,22 @@ summarise_ecosystem_features_Baker <- function(parameters,sim_args){
   M <- A-P
 
   fn <- function(N) {
-    output <- r*(1-exp(-M%*%N-P))-B%*%N
+    output <- R*(1-exp(-M%*%N-P))-B%*%N
     return(output)
   }
 
   sol <- nleqslv::nleqslv(rep(0,n_species), fn)
-  equilibrium_points <- solve(A,-r)
 
   # STABILITY CHECK  TODO
   #calculate Jacobian for stability ###CHECK THIS PROCESS
-  jacobian <- A*matrix(equilibrium_points, ncol=n_species, nrow=n_species)
-  diag(jacobian) <- diag(jacobian) + A%*%(equilibrium_points) + r
+  diag_elements <- -diag(B)*N_sol
+
+  R_matrix <- matrix(R, ncol = length(R), nrow = length(R))
+  N_matrix <- matrix(N_sol, ncol = length(N_sol), nrow = length(N_sol))
+
+  jabobian <- R_matrix*N_matrix*A*exp(-M%*%N_sol-P)-B*N_matrix*t(N_matrix)
+
+  diag(jabobian) <- diag_elements
   #check stability
   stability_eigenvalues <- Re(eigen(jacobian)$values)
 
--- tests/testthat/test-EEM.R
+++ tests/testthat/test-EEM.R
@@ -1,3 +1,3 @@
-test_that("multiplication works", {
+test_that("EEM tests", {
   expect_equal(2 * 2, 4)
 })
