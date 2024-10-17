c6d68ba5b8dd2c4077531a98f792dd0ac458ca0a
luzvpascal <luz.pascal96@gmail.com>
2024-10-17 11:07:57 +1000

    adding scaled projections

--- R/plot_projections.R
+++ R/plot_projections.R
@@ -2,11 +2,12 @@
 #' @description
 #' Solves ODEs and plot solutions
 #' @param parameters list like object of ensemble of parameters (outputs of  \link[EEMtoolbox]{EEM})
-#' @param initial_condition vector of initial species abundances
+#' @param initial_condition vector of initial species abundances. If parameter scaled is TRUE, the parameter initial_condition should be scaled to steady state
 #' @param t_window time window to solve ODE
 #' @param time_step_len length of each time step, default = 0.01
 #' @param model model representing species interactions. Default "GLV" (Generalized Lotka Volterra). options include "Baker" and "Gompertz".
 #' @param derivative derivative function. Default \link[EEMtoolbox]{derivative_func}
+#' @param scaled Boolean indicating if projections should be scaled to steady state. If true, the parameter initial_condition should be scaled too. Default FALSE
 #' @param species_names vector of strings for names of species. If NA plots only display species index number, . Default NA.
 #' @param average Boolean indicating if projections should be averaged, or indivially ploted
 #' @examples
@@ -19,8 +20,10 @@ plot_projections <- function(parameters,
                         initial_condition,
                         t_window,
                         time_step_len=0.01,
+                        sim_args,
                         model = "GLV",
                         derivative=EEMtoolbox::derivative_func,
+                        scaled=FALSE,
                         species_names=NA,
                         average=TRUE){
 
@@ -29,34 +32,71 @@ plot_projections <- function(parameters,
                            t_window,
                            time_step_len,
                            model,
-                           derivative){
-    if (model != "Baker"){
-      return(as.data.frame(EEMtoolbox::ode_solve(interaction_matrix_value = pars$interaction_matrix,
-                                 growth_rate=pars$growthrates,
-                                 initial_condition,
-                                 t_window,
-                                 time_step_len,
-                                 model,
-                                 derivative)))
+                           derivative,
+                           scaled){
+    #if scaled: find steady state, initial condition is scaled to steady state
+    if (scaled) {
+      if (model %in% c("GLV", "Gompertz")){
+        steady_state <- solve(pars$interaction_matrix,-pars$growthrates)
+        if (model=="Gompertz"){
+          steady_state <- exp(steady_state)
+        }
+      } else if (model =="Baker"){
+        r <- pars$growthrates
+        A <- pars$interaction_matrix_alphas
+        B <- pars$interaction_matrix_betas
+
+        R <- r
+        P <- diag(A)
+        M <- A-diag(P)
+
+        fn <- function(N) {
+          output <- R*(1-exp(-M%*%N-P))+B%*%N #change to positive
+          return(output)
+        }
+        sol <- nleqslv::nleqslv(rep(100,n_species), fn) #we give a large positive warmstart
+        steady_state <- sol$x
+      }
+      initial <- initial_condition*steady_state #initial condition is scaled to steady state
     } else {
-      return(as.data.frame(EEMtoolbox::ode_solve(interaction_matrix_value =
-                                                   list(pars$interaction_matrix_alphas,
-                                                        pars$interaction_matrix_betas),
-                                                 growth_rate=pars$growthrates,
-                                                 initial_condition,
-                                                 t_window,
-                                                 time_step_len,
-                                                 model,
-                                                 derivative)))
+      initial <- initial_condition #non-scaled to steady state
+    }
+    ## solve ODE
+    if (model %in% c("GLV", "Gompertz")){
+      projections <- as.data.frame(EEMtoolbox::ode_solve(interaction_matrix_value = pars$interaction_matrix,
+                                                         growth_rate=pars$growthrates,
+                                                         initial,
+                                                         t_window,
+                                                         time_step_len,
+                                                         model,
+                                                         derivative))
+    } else if (model =="Baker") {
+      projections <-as.data.frame(EEMtoolbox::ode_solve(interaction_matrix_value =
+                                            list(pars$interaction_matrix_alphas,
+                                                 pars$interaction_matrix_betas),
+                                          growth_rate=pars$growthrates,
+                                          initial,
+                                          t_window,
+                                          time_step_len,
+                                          model,
+                                          derivative))
     }
 
+    if (scaled){#if scaled to steady state, scale abundances
+      projections[,-1] <- projections[,-1]/matrix(steady_state,
+                                                  ncol=length(steady_state),
+                                                  nrow=nrow(projections),
+                                                  byrow=TRUE)
+    }
+    return(projections)
   }
 
   abundance <- lapply(parameters,ode_solve_it, model=model,
                                             initial_condition=initial_condition,
                                             t_window=t_window,
                                             time_step_len=time_step_len,
-                                            derivative)
+                                            derivative,
+                                            scaled)
 
   abundance <- dplyr::bind_rows(abundance, .id="sim")
   if (!is.na(species_names[1])){
