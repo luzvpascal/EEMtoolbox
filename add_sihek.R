96a650f405627974dda5c8fe462bfb5a1c7c26b3
maude-v <maude.vernet@students.unibe.ch>
2025-03-06 18:08:23 +0100

    New version with K to add_introduced_species
    Also a way to check which eq abundance are in the correct range of the introduced K

--- add_sihek.R
+++ add_sihek.R
@@ -1,4 +1,5 @@
 source("add_introduced_species.R")
+load("parameters_GLV_nosihek_adapteddiscfunc_200.RData")
 
 ###### add the introduced species to parameter sets produced by EEM #####
 parameters_GLV_plussishek_adapteddiscfunc_200 <-
@@ -9,51 +10,51 @@ parameters_GLV_plussishek_adapteddiscfunc_200 <-
                          introduced_self_sign = -1,
                          introduced_row_signs = c(1, 1, 1, 1, 1, 1, 1, 1),
                          introduced_col_signs = c(-1, -1, -1, -1, -1, -1, -1, 0),
-                         interaction_strength_upper_bounds = c(0.5, 0.5, 1, 1,
-                                                               0.5, 0.1, 0.1,
-                                                               1, 1),
-                         interaction_strength_lower_bounds = c(0, 0, 0, 0, 0,
-                                                               0, 0, 0, 0))
+                         introduced_k = 40/20000000)
 
 #extract species names
 species_names <- c(colnames(parameters_GLV_plussishek_adapteddiscfunc[[1]]$interaction_matrix)[1:8], "sihek")
 
-##### check the abundance of sihek #####
-interaction_matrix_with_sihek <- matrix(c(-1, 0, -1, 0, 0, 0, 0, 1, -1,
-                                          0, -1, -1, 0, 0, 0, 0, 1, -1,
-                                          1, 1, -1, 0, 0, 1, 1, 0, -1,
-                                          0, 0, 0, -1, 1, 1, 1, 0, -1,
-                                          0, 0, 0, -1, -1, 0, 1, 0, -1,
-                                          0, 0, -1, -1, 0, -1, 0, 1, -1,
-                                          0, 0, -1, -1, -1, 0, -1, 1, -1,
-                                          1, -1, -1, 0, 0, 0, 1, -1, 0,
-                                          1, 1, 1, 1, 1, 1, 1, 1, -1),
-                                        ncol = 9, nrow = 9, byrow = TRUE)
+##### check the equilibrium abundance of sihek --> not the exact number but the order of size --> eg same number of decimals#####
+eq_which <- which(sapply(parameters_GLV_plussishek_adapteddiscfunc_200, function(x) x$eq_intro_abundance) > 0)
+for (i in 1:5000) {
+  r <- parameters_GLV_plussishek_adapteddiscfunc_200[[i]]$growthrates
+  A <- parameters_GLV_plussishek_adapteddiscfunc_200[[i]]$interaction_matrix
+  equilibrium <- solve(A, -r)
+  if ((40/20000000)/equilibrium[1] > 0.1
+      && (40/20000000)/equilibrium[1] < 10) {
+    eq[[i]]$eq_param_number <- i
+    eq[[i]]$eq_intro_abundance <- equilibrium[1]
+  }
+}
 
-arg_withsihek <- args_function(
-  interaction_matrix = interaction_matrix_with_sihek,
-  upper_bounds_growth_rate = c(upper_bounds_growth_rate_nosihek, 1.1),
-  model = "GLV")
-
-# check the equilibrium for one parameter set
-reconstructed_withsihek <-
-  reconstruct_matrix_growthrates(parameters_GLV_plussishek_adapteddiscfunc_200[[1]],
-                                 arg_withsihek)
-
-r <- reconstructed_withsihek$growthrates$growthrates
-A <- reconstructed_withsihek$growthrates$interaction_matrix
-equilibrium <- solve(A, -r)
+eq <- c()
+non_eq <- c()
+for (i in 1:5000) {
+  r <- parameters_GLV_plussishek_adapteddiscfunc_200[[i]]$growthrates
+  A <- parameters_GLV_plussishek_adapteddiscfunc_200[[i]]$interaction_matrix
+  equilibrium <- solve(A, -r)
+  if ((40/20000000)/equilibrium[1] > 0.1
+      && (40/20000000)/equilibrium[1] < 10) {
+    eq[i] <- equilibrium[1]
+  } else {
+    non_eq[i] <- equilibrium[1]
+  }
+}
+eq <- eq[!sapply(eq, is.na)]
+non_eq <- non_eq[!sapply(non_eq, is.na)]
+eq
+non_eq
 
 ##### ------> find a way to: select the values where all abundances are at least 0 ? set something where if abundance is negative, set to 0?? is it needed? can we adapt that in the plot_projection function by adapting the derivative_func?
-##### ------> find a way to set abundance of sihek to 9 when all other abundances are set to 1 from the nosihek case
-##### ------> run without sihek first, save abundance of all species, take them as initial conditions for the rest?
+##### ------> find a way to set abundance of sihek to 9 when all other abundances are set to 1 from the nosihek case --> this is done --> set eq abundance to K and then percentage of this
+##### ------> run without sihek first, save abundance of all species, take them as initial conditions for the rest? --> not needed since taking the eq abundance without sihek? noo! the eq abundances are not the same since sihek is in there. the one should be the 1 of the first EEM(). can we extract that? Yes! because we set them
 
 
 ##### plot projections with sihek in the model #####
 plot_projections(parameters = parameters_GLV_plussishek_adapteddiscfunc_200,
-                 initial_condition = c(1, 1, 1, 1, 1, 1, 1, 1,
-                                       6/40),
-                 t_window = c(0,20),
+                 initial_condition = c(6/40, 1, 1, 1, 1, 1, 1, 1, 1),
+                 t_window = c(0,10),
                  scaled = TRUE,
                  species_names = species_names)
 
