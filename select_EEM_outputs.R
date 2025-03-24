1b9b5b5d2b780902ddee4f46705a9548ca6b6958
maude-v <maude.vernet@students.unibe.ch>
2025-03-24 16:36:36 +0100

    update function to plot the results

--- select_EEM_outputs.R
+++ select_EEM_outputs.R
@@ -9,6 +9,7 @@ select_EEM_outputs <- function(ensemble,
 
   # Prepare an empty list to collect selected parameter sets
   selected <- list()
+  unselected <- list()
 
   for (i in seq_along(ensemble)) {
     param_set <- ensemble[[i]]
@@ -37,9 +38,44 @@ select_EEM_outputs <- function(ensemble,
     # If the equilibrium is within the bounds, select this parameter set.
     if (length(which(current_eq > target_lower)) == sim_args$n_species &&
         length(which(current_eq < target_upper)) == sim_args$n_species) {
-      selected[[length(selected) + 1]] <- param_set
+      selected[[length(selected) + 1]] <-
+        list(param_set, data.frame("equilibrium" = as.vector(current_eq, mode = "numeric"),
+                                   "species" = 1:sim_args$n_species))
+    } else {
+      unselected[[length(unselected) + 1]] <-
+        list(param_set, data.frame("equilibrium" = as.vector(current_eq, mode = "numeric"),
+                                   "species" = 1:sim_args$n_species))
     }
   }
+  if (length(selected) == 0) {
+    cat("No parameter sets found within the bounds.\n")
+    df <- data.frame(equilibrium = numeric(), species = numeric())
+    for (i in c(1:length(unselected))) {
+      df <- data.frame(equilibrium = c(df$equilibrium,
+                                       unselected[[i]][[2]]$equilibrium),
+                       species = c(df$species,
+                                   unselected[[i]][[2]]$species))
+    }
+  } else {
+    cat("Selected", length(selected), "parameter sets.\n")
+  df <- data.frame(equilibrium = numeric(), species = numeric())
+  for (i in c(1:length(selected))) {
+    df <- data.frame(equilibrium = c(df$equilibrium,
+                                     selected[[i]][[2]]$equilibrium),
+                     species = c(df$species,
+                                 selected[[i]][[2]]$species))
+  }
+  }
+  uf <- data.frame(upper = target_upper, species = unique(df$species))
+  lf <- data.frame(lower = target_lower, species = unique(df$species))
+  a <- ggplot2::ggplot() +
+    ggplot2::geom_point(data = df, ggplot2::aes(x = species, y = equilibrium,
+                                                color = "equilibrium")) +
+    ggplot2::geom_point(data = uf, ggplot2::aes(x = species, y = upper,
+                                                color = "upper")) +
+    ggplot2::geom_point(data = lf, ggplot2::aes(x = species, y = lower,
+                                                color = "lower")) +
+    ggplot2::labs(x = "Species", y = "Equilibrium", colors = "Legends")
 
-  return(selected)
+  print(a)
 }
